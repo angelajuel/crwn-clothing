8a4c58646acafd1c97cd49dbaf60db1c3ebfeb02
angelajuel <angelajuel12@gmail.com>
2020-08-31 16:25:12 -0500

    Added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -19,6 +19,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 // spread in already existing array values and newly added values
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems(cartItem =>
+                    cartItem.id !== action.payload.id
+                )
             };
         default:
             return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, {...cartItemToAdd, quantity: 1 }]
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => 
+        cartItem.id === cartItemToRemove.id
+    );
+
+    // if the cartItemToRemove's quantity is 1, then remove it
+    if(existingCartItem.quantity === 1) {
+        cartItems.filter( 
+            cartItem => cartItem.id !== cartItemToRemove.id
+        );
+    }
+
+    // else, decrease the cart item's quantity and keep every other cart item the exact same 
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id 
+            ? { ...cartItem, quantity: cartItem.quantity - 1}
+            : { cartItem }
+    );
+};
